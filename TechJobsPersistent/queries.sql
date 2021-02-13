bbd2303559a2f8952619feb9dd3e79a21eefb8b4
ca-tzi <68138133+ca-tzi@users.noreply.github.com>
2021-02-13 14:21:24 -0500

    finished

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -7,13 +7,7 @@ FROM INFORMATION_SCHEMA.COLUMNS where (table_name = "jobs")
 --Part 2
 select column_name from employers where (column_name = "St. Louis")
 --Part 3
-SELECT name, description FROM skills
+SELECT * FROM skills
 LEFT JOIN jobskills ON skills.Id = jobskills.SkillId
 WHERE jobskills.JobId is not null
-
-UNION
-
-SELECT name FROM jobs
-LEFT JOIN jobskills ON jobs.Id = jobskills.JobId
-WHERE jobskills.JobId is not null
-ORDER BY name asc;
+order by name asc;
